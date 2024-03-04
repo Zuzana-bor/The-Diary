9c0d4c1a80c2dc76cb90b2133513fb4910631d77
Zuzana-bor <z.kasikova@gmail.com>
2024-03-04 13:44:45 +0100

    wip

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,18 +1,20 @@
 const Sidebar = () => {
   return (
     <div className="border-r-4 w-full max-w-96 h-screen ms:hidden bg-stone-300">
-      <div className="flex justify-between mx-5">
+      <div className="flex justify-between p-5">
         <h1 className="text-6xl">Notes</h1>
 
-        <button className="text-cyan-500 text-2xl border-solid border-2 border-sky-500 ">
+        <button className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded">
           Add
         </button>
       </div>
       <div>
         <div className="bg-stone-500">
-          <div>
-            <strong>TITLE</strong>
-            <button>DELETE</button>
+          <div className="flex justify-between p-5">
+            <strong className="text-3xl">TITLE</strong>
+            <button className="bg-red-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded">
+              DELETE
+            </button>
           </div>
           <p>Note preview</p>
           <small>Last modified [date]</small>
