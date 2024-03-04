407420b2e3fbdb8e76a2e6ea6f2cc9105421a85a
Zuzana-bor <z.kasikova@gmail.com>
2024-03-04 20:56:58 +0100

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import './App.css';
+import MainArea from './MainArea';
 import Sidebar from './Sidebar';
 
 function App() {
   return (
-    <>
+    <div className="flex">
       <Sidebar />
-    </>
+      <MainArea />
+    </div>
   );
 }
 
--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,6 +1,6 @@
 const Sidebar = () => {
   return (
-    <div className="border-r-4 w-full max-w-96 h-screen ms:hidden bg-stone-300">
+    <div className="border-r-4 w-full max-w-96 min-h-dvh ms:hidden bg-stone-300">
       <div className="flex justify-between p-5">
         <h1 className="text-6xl">Notes</h1>
 
@@ -9,8 +9,8 @@ const Sidebar = () => {
         </button>
       </div>
       <div>
-        <div className="bg-stone-500">
-          <div className="flex justify-between p-5">
+        <div className="bg-stone-400 hover:bg-stone-500 p-5">
+          <div className="flex justify-between ">
             <strong className="text-3xl">TITLE</strong>
             <button className="bg-red-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded">
               DELETE
